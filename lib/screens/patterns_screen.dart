7bc290f522cbdf521693b74a32369307a60bd6ea
pasi-ya <116879193+pasi-ya@users.noreply.github.com>
2025-08-09 19:50:26 +0530

    add some screens (quiz, more, fav)

--- lib/screens/patterns_screen.dart
+++ lib/screens/patterns_screen.dart
@@ -110,3 +110,45 @@ class _PatternsScreenState extends State<PatternsScreen> {
     );
   }
 }
+
+class QuizScreen extends StatelessWidget {
+  const QuizScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0D1117),
+      body: Center(
+        child: Text('Quiz Screen', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class FavoritesScreen extends StatelessWidget {
+  const FavoritesScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0D1117),
+      body: Center(
+        child: Text('Favourite Screen', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
+
+class MoreScreen extends StatelessWidget {
+  const MoreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFF0D1117),
+      body: Center(
+        child: Text('More Screen', style: TextStyle(color: Colors.white)),
+      ),
+    );
+  }
+}
